49d3a577cfb07be2cdf5eadb0701e1f57a29e1a3
Michael Cawte <mcawte@gmail.com>
2023-07-11 23:39:55 +1000

    New icon

--- src/utilities.ts
+++ src/utilities.ts
@@ -9,12 +9,12 @@ export function formatDuration(duration: number) {
 }
 
 export function convertDataToCSV(meetings: MeetingData): string {
-  let csvStr = "URL,Start Time,End Time,Duration,Participants\n"; // CSV header
+  let csvStr = "Title,Start Time,End Time,Duration,Participants\n"; // CSV header
 
   for (const url in meetings) {
     for (const meeting of meetings[url]) {
       let durationStr = formatDuration(meeting.duration);
-      csvStr += `${url},${meeting.startTime},${
+      csvStr += `${meeting.title},${meeting.startTime},${
         meeting.endTime
       },"${durationStr}","${meeting.participants.join(", ")}"\n`;
     }
